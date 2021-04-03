977a9ad3d7feb02b281986b32724f1fe3a835a87
Johannes Gustafsson <johannesgu@gmail.com>
2021-04-03 10:15:43 +0200

    client

--- app/client/src/apollo/cache.ts
+++ app/client/src/apollo/cache.ts
@@ -1,3 +1,6 @@
-import { InMemoryCache } from "@apollo/client";
+import { InMemoryCache, makeVar } from "@apollo/client";
+import { Instrument } from "./types";
 
 export const cache = new InMemoryCache();
+
+export const currentInstrument = makeVar<Instrument>({ id: "", name: ""});
--- app/client/src/components/SelectInstrument.tsx
+++ app/client/src/components/SelectInstrument.tsx
@@ -1,19 +1,26 @@
 import MenuItem from "@material-ui/core/MenuItem";
 import Select from "@material-ui/core/Select";
 import React from "react";
-import { useGetInstrumentsQuery } from "../apollo/types";
-
+import { Instrument, useGetInstrumentsQuery } from "../apollo/types";
+import { currentInstrument } from '../apollo/cache';
 
 export function SelectInstrument() {
     const { loading, error, data } = useGetInstrumentsQuery();
 
+    const handleSelect = (event: React.ChangeEvent<{ value: unknown }>) => {
+        currentInstrument(event.target.value as Instrument);
+      };
+    
+
     return <Select
         labelId="demo-simple-select-label"
         id="demo-simple-select"
+        value={currentInstrument()}
+        onChange={handleSelect}
     >
         {loading ?
             <MenuItem value={0}>Loading...</MenuItem>
-            : data?.instruments?.map(i => <MenuItem value={i.id}>{i.name}</MenuItem>
+            : data?.instruments?.map(i => <MenuItem value={i?.id}>{i?.name}</MenuItem>
             )
         }
     </Select>
