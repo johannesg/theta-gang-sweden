93a1b861aab7e689bde5731bada0435e0773a4b2
Johannes Gustafsson <johannesgu@gmail.com>
2021-04-21 18:46:04 +0200

    tests

--- app/lambda/test/calc-greeks.test.ts
+++ app/lambda/test/calc-greeks.test.ts
@@ -10,44 +10,44 @@ describe("calculate greeks", () => {
     test("price", () => {
         expect(calc_price_call(underlying, underlying - 5, dte, vol, interest)).toBe(5.088380000200118);
         expect(calc_price_put(underlying, underlying - 5, dte, vol, interest)).toBe(0.07358791136165532);
-        expect(calc_price_call(underlying, underlying, dte, vol, interest)).not.toBeNaN();
-        expect(calc_price_put(underlying, underlying, dte, vol, interest)).not.toBeNaN();
-        expect(calc_price_call(underlying, underlying + 5, dte, vol, interest)).not.toBeNaN();
-        expect(calc_price_put(underlying, underlying + 5, dte, vol, interest)).not.toBeNaN();
+        expect(calc_price_call(underlying, underlying, dte, vol, interest)).toBe(1.3284438205903086);
+        expect(calc_price_put(underlying, underlying, dte, vol, interest)).toBe(1.3120081663253593);
+        expect(calc_price_call(underlying, underlying + 5, dte, vol, interest)).toBe(0.11785618156992461);
+        expect(calc_price_put(underlying, underlying + 5, dte, vol, interest)).toBe(5.099776961878476);
     });
     test("delta", () => {
         expect(calc_delta_call(underlying, underlying - 5, dte, vol, interest)).toBe(0.9483873825790621);
         expect(calc_delta_put(underlying, underlying - 5, dte, vol, interest)).toBe(-0.05161261742093792);
-        expect(calc_delta_call(underlying, underlying, dte, vol, interest)).not.toBeNaN();
-        expect(calc_delta_put(underlying, underlying, dte, vol, interest)).not.toBeNaN();
-        expect(calc_delta_call(underlying, underlying + 5, dte, vol, interest)).not.toBeNaN();
-        expect(calc_delta_put(underlying, underlying + 5, dte, vol, interest)).not.toBeNaN();
+        expect(calc_delta_call(underlying, underlying, dte, vol, interest)).toBe(0.5151840132766417);
+        expect(calc_delta_put(underlying, underlying, dte, vol, interest)).toBe(-0.48481598672335835);
+        expect(calc_delta_call(underlying, underlying + 5, dte, vol, interest)).toBe(0.0805357799996716);
+        expect(calc_delta_put(underlying, underlying + 5, dte, vol, interest)).toBe(-0.9194642200003285);
     });
     test("gamma", () => {
         expect(calc_gamma(underlying, underlying - 5, dte, vol, interest)).toBe(0.03195231728952847);
-        expect(calc_gamma(underlying, underlying, dte, vol, interest)).not.toBeNaN();
-        expect(calc_gamma(underlying, underlying + 5, dte, vol, interest)).not.toBeNaN();
+        expect(calc_gamma(underlying, underlying, dte, vol, interest)).toBe(0.12042370119920676);
+        expect(calc_gamma(underlying, underlying + 5, dte, vol, interest)).toBe(0.045135654795373105);
     });
     test("theta", () => {
         expect(calc_theta_call(underlying, underlying - 5, dte, vol, interest)).toBe(-0.004724955703733377);
         expect(calc_theta_put(underlying, underlying - 5, dte, vol, interest)).toBe(-0.004355214268847118);
-        expect(calc_theta_call(underlying, underlying, dte, vol, interest)).not.toBeNaN();
-        expect(calc_theta_put(underlying, underlying, dte, vol, interest)).not.toBeNaN();
-        expect(calc_theta_call(underlying, underlying + 5, dte, vol, interest)).not.toBeNaN();
-        expect(calc_theta_put(underlying, underlying + 5, dte, vol, interest)).not.toBeNaN();
+        expect(calc_theta_call(underlying, underlying, dte, vol, interest)).toBe(-0.0166971981657262);
+        expect(calc_theta_put(underlying, underlying, dte, vol, interest)).toBe(-0.016286374349185912);
+        expect(calc_theta_call(underlying, underlying + 5, dte, vol, interest)).toBe(-0.006215094625270948);
+        expect(calc_theta_put(underlying, underlying + 5, dte, vol, interest)).toBe(-0.005763188427076632);
     });
     test("vega", () => {
         expect(calc_vega(underlying, underlying - 5, dte, vol, interest)).toBe(0.017508119062755328);
-        expect(calc_vega(underlying, underlying, dte, vol, interest)).not.toBeNaN();
-        expect(calc_vega(underlying, underlying + 5, dte, vol, interest)).not.toBeNaN();
+        expect(calc_vega(underlying, underlying, dte, vol, interest)).toBe(0.06598558969819548);
+        expect(calc_vega(underlying, underlying + 5, dte, vol, interest)).toBe(0.024731865641300334);
     });
     test("rho", () => {
         expect(calc_rho_call(underlying, underlying - 5, dte, vol, interest)).toBe(0.046390125072606016);
         expect(calc_rho_put(underlying, underlying - 5, dte, vol, interest)).toBe(-0.002908732912228553);
-        expect(calc_rho_call(underlying, underlying, dte, vol, interest)).not.toBeNaN();
-        expect(calc_rho_put(underlying, underlying, dte, vol, interest)).not.toBeNaN();
-        expect(calc_rho_call(underlying, underlying + 5, dte, vol, interest)).not.toBeNaN();
-        expect(calc_rho_put(underlying, underlying + 5, dte, vol, interest)).not.toBeNaN();
+        expect(calc_rho_call(underlying, underlying, dte, vol, interest)).toBe(0.02677343215697728);
+        expect(calc_rho_put(underlying, underlying, dte, vol, interest)).toBe(-0.028003076715061116);
+        expect(calc_rho_call(underlying, underlying + 5, dte, vol, interest)).toBe(0.004283761992782088);
+        expect(calc_rho_put(underlying, underlying + 5, dte, vol, interest)).toBe(-0.05597039776646016);
     });
 });
 
