b0b9490c38c4203fcf2efb2223d2a4b0ba854398
Johannes Gustafsson <johannes@jogus.io>
2021-04-21 14:42:15 +0200

    Consolidating funcgtions

--- app/lambda/src/utils/calc-greeks.ts
+++ app/lambda/src/utils/calc-greeks.ts
@@ -19,9 +19,15 @@ function norm_pdf(z: number) {
     return jStat.normal.pdf(z, mean, sd);
 }
 
-function d(vol: number, S: number, K: number, r: number, t: number) : [ number, number ] {
-    const d1 = 1 / (vol * sqrt(t)) * ( log(S/K) + (r + pow(vol, 2) / 2) * t);
-    const d2 = d1 - vol * sqrt(t);
+// S = underlying price
+// K = strike
+// t = time to expiration in years
+// r = risk free interest
+// sigma = implied volatility
+
+function d(sigma: number, S: number, K: number, r: number, t: number): [number, number] {
+    const d1 = 1 / (sigma * sqrt(t)) * (log(S / K) + (r + pow(sigma, 2) / 2) * t);
+    const d2 = d1 - sigma * sqrt(t);
     return [d1, d2];
 }
 
@@ -43,79 +49,116 @@ function d_two(underlying: number, strike: number, dte: number, vol: number, int
     return d_one(underlying, strike, dte, vol, interest) - vol * sqrt(dte)
 }
 
-export function calc_price_call(underlying: number, strike: number, dte: number, vol: number, interest: number): number {
-    dte = dte / 365
-    return norm_cdf(d_one(underlying, strike, dte, vol, interest)) * underlying - norm_cdf(d_two(underlying, strike, dte, vol, interest)) * strike * exp(-interest * dte)
-}
-
-export function calc_price_put(underlying: number, strike: number, dte: number, vol: number, interest: number): number {
-    dte = dte / 365
-    return norm_cdf(-d_two(underlying, strike, dte, vol, interest)) * strike * exp((-interest) * dte) - norm_cdf(-d_one(underlying, strike, dte, vol, interest)) * underlying
-}
-
-export function calc_delta_call(underlying: number, strike: number, dte: number, vol: number, interest: number): number {
-    dte = dte / 365;
-    return norm_cdf(d_one(underlying, strike, dte, vol, interest))
-}
-
-export function calc_delta_put(underlying: number, strike: number, dte: number, vol: number, interest: number): number {
-    dte = dte / 365
-    return norm_cdf(d_one(underlying, strike, dte, vol, interest)) - 1
-}
-
-export function calc_gamma(underlying: number, strike: number, dte: number, vol: number, interest: number): number {
-    dte = dte / 365
-    return Nd_one(underlying, strike, dte, vol, interest) / (underlying * (vol * sqrt(dte)))
-}
-
-export function calc_vega(underlying: number, strike: number, dte: number, vol: number, interest: number): number {
-    dte = dte / 365
-    return underlying * Nd_one(underlying, strike, dte, vol, interest) * sqrt(dte) * 0.01
-}
-
-export function calc_rho_call(underlying: number, strike: number, dte: number, vol: number, interest: number): number {
-    dte = dte / 365
-    return strike * dte * exp(-interest * dte) * norm_cdf(d_two(underlying, strike, dte, vol, interest)) * 0.01
-}
-
-export function calc_rho_put(underlying: number, strike: number, dte: number, vol: number, interest: number): number {
-    dte = dte / 365
-    return -strike * dte * exp(-interest * dte) * norm_cdf(-d_two(underlying, strike, dte, vol, interest)) * 0.01
-}
-
-export function calc_theta_call(underlying: number, strike: number, dte: number, vol: number, interest: number): number {
-    dte = dte / 365
-    return (-(underlying * vol * Nd_one(underlying, strike, dte, vol, interest)) / (2 * sqrt(dte)) - interest * strike * exp(-interest * (dte)) * norm_cdf(d_two(underlying, strike, dte, vol, interest))) / 365
-}
-
-export function calc_theta_put(underlying: number, strike: number, dte: number, vol: number, interest: number): number {
-    dte = dte / 365
-    return (-(underlying * vol * Nd_one(underlying, strike, dte, vol, interest)) / (2 * sqrt(dte)) + interest * strike * exp(-interest * (dte)) * norm_cdf(-d_two(underlying, strike, dte, vol, interest))) / 365
-}
-
-function call_price(sigma: number, S: number, K: number, r: number, t: number, d1: number, d2: number) : number {
+function _calc_price_call(S: number, K: number, r: number, t: number, d1: number, d2: number): number {
     return norm_cdf(d1) * S - norm_cdf(d2) * K * exp(-r * t);
 }
 
-export function calc_iv(underlying: number, strike: number, dte: number, interest: number, callPrice: number) : number {
-    const [ S, K, r, t, C0 ] = [ underlying, strike, interest, dte / 365, callPrice ];
+function _calc_price_put(S: number, K: number, r: number, t: number, d1: number, d2: number): number {
+    return -norm_cdf(-d1) * S + norm_cdf(-d2) * K * exp(-r * t) ;
+}
 
-    const [abstol, max_iter] = [1e-4, 1e3];
+export function calc_price_call(underlying: number, strike: number, dte: number, vol: number, interest: number): number {
+    const t = dte / 365;
+    const [d1, d2] = d(vol, underlying, strike, interest, t);
+    return _calc_price_call(underlying, strike, interest, t, d1, d2);
+}
+
+export function calc_price_put(underlying: number, strike: number, dte: number, vol: number, interest: number): number {
+    const t = dte / 365;
+    const [d1, d2] = d(vol, underlying, strike, interest, t);
+    return _calc_price_put(underlying, strike, t, interest, d1, d2);
+}
+
+export function calc_delta_call(underlying: number, strike: number, dte: number, vol: number, interest: number): number {
+    const t = dte / 365;
+    const [d1, _] = d(vol, underlying, strike, interest, t);
+    return norm_cdf(d1);
+}
+
+export function calc_delta_put(underlying: number, strike: number, dte: number, vol: number, interest: number): number {
+    const t = dte / 365;
+    const [d1, _] = d(vol, underlying, strike, interest, t);
+    return norm_cdf(d1) - 1
+}
+
+export function calc_gamma(underlying: number, strike: number, dte: number, vol: number, interest: number): number {
+    const t = dte / 365
+    return Nd_one(underlying, strike, t, vol, interest) / (underlying * (vol * sqrt(t)))
+}
+
+function vega(sigma: number, S: number, K: number, r: number, t: number): number {
+    const [d1, d2] = d(sigma, S, K, r, t);
+    const v = S * norm_pdf(d1) * sqrt(t);
+    return v;
+}
+
+export function calc_vega(underlying: number, strike: number, dte: number, vol: number, interest: number): number {
+    const t = dte / 365
+    return underlying * Nd_one(underlying, strike, t, vol, interest) * sqrt(t) * 0.01
+        // const vega = S * norm_pdf(d1) * sqrt(t);
+        // return vega(vol, underlying, strike, interest, t);
+
+    // const a = pow(d_one(underlying, strike, dte, vol, interest), 2);
+    // const b = sqrt(2 * 3.14159);
+    // return exp(-a / 2) / (b);
+}
+
+export function calc_rho_call(underlying: number, strike: number, dte: number, vol: number, interest: number): number {
+    const t = dte / 365;
+    const [_, d2] = d(vol, underlying, strike, interest, t);
+    return strike * t * exp(-interest * t) * norm_cdf(d2) * 0.01
+}
+
+export function calc_rho_put(underlying: number, strike: number, dte: number, vol: number, interest: number): number {
+    const t = dte / 365;
+    const [_, d2] = d(vol, underlying, strike, interest, t);
+    return -strike * t * exp(-interest * t) * norm_cdf(-d2) * 0.01
+}
+
+export function calc_theta_call(underlying: number, strike: number, dte: number, vol: number, interest: number): number {
+    const t = dte / 365;
+    const [_, d2] = d(vol, underlying, strike, interest, t);
+    return (-(underlying * vol * Nd_one(underlying, strike, t, vol, interest))
+        / (2 * sqrt(t)) - interest * strike * exp(-interest * (t)) 
+        * norm_cdf(d2)) / 365
+}
+
+export function calc_theta_put(underlying: number, strike: number, dte: number, vol: number, interest: number): number {
+    const t = dte / 365;
+    const [_, d2] = d(vol, underlying, strike, interest, t);
+    return (-(underlying * vol * Nd_one(underlying, strike, t, vol, interest))
+        / (2 * sqrt(t)) + interest * strike * exp(-interest * (t))
+        * norm_cdf(-d2)) / 365
+}
+
+function _calc_iv(underlying: number, strike: number, dte: number, interest: number,
+     price: number, pricefn: Function): number {
+    const [S, K, r, t] = [underlying, strike, interest, dte / 365];
+
+    const [tol, max_iter] = [1e-4, 1e3];
 
     let [epsilon, i, vol] = [1.0, 0, 0.5];
 
-    while (epsilon > abstol) {
+    while (epsilon > tol) {
         if (i > max_iter)
             throw "Failed to find root. Too many iterations";
 
         i++;
-        const orig = vol;
+        const orig_vol = vol;
         const [d1, d2] = d(vol, S, K, r, t);
-        const fun_val = call_price(vol, S, K, r, t, d1, d2) - C0;
+        const fun_val = pricefn(S, K, r, t, d1, d2) - price;
         const vega = S * norm_pdf(d1) * sqrt(t);
         vol = -fun_val / vega + vol;
-        epsilon = abs(fun_val);
+        epsilon = abs( (vol - orig_vol) / orig_vol);
     }
 
     return vol;
+}
+
+export function calc_iv_call(underlying: number, strike: number, dte: number, interest: number, price: number): number {
+    return _calc_iv(underlying, strike, dte, interest, price, _calc_price_call);
+}
+
+export function calc_iv_put(underlying: number, strike: number, dte: number, interest: number, price: number): number {
+    return _calc_iv(underlying, strike, dte, interest, price, _calc_price_put);
 }
--- app/lambda/test/calc-greeks.test.ts
+++ app/lambda/test/calc-greeks.test.ts
@@ -1,4 +1,4 @@
-import { calc_delta_call, calc_delta_put, calc_gamma, calc_iv, calc_price_call, calc_price_put, calc_rho_call, calc_rho_put, calc_theta_call, calc_theta_put, calc_vega } from '../src/utils/calc-greeks';
+import { calc_delta_call, calc_delta_put, calc_gamma, calc_iv_call, calc_iv_put, calc_price_call, calc_price_put, calc_rho_call, calc_rho_put, calc_theta_call, calc_theta_put, calc_vega } from '../src/utils/calc-greeks';
 
 describe("calculate greeks", () => {
     const underlying = 50;
@@ -8,26 +8,46 @@ describe("calculate greeks", () => {
     const interest = 0.003;
 
     test("price", () => {
-        expect(calc_price_call(underlying, strike, dte, vol, interest)).toBe(5.088380000200118);
-        expect(calc_price_put(underlying, strike, dte, vol, interest)).toBe(0.07358791136165532);
+        expect(calc_price_call(underlying, underlying - 5, dte, vol, interest)).toBe(5.088380000200118);
+        expect(calc_price_put(underlying, underlying - 5, dte, vol, interest)).toBe(0.07358791136165532);
+        expect(calc_price_call(underlying, underlying, dte, vol, interest)).not.toBeNaN();
+        expect(calc_price_put(underlying, underlying, dte, vol, interest)).not.toBeNaN();
+        expect(calc_price_call(underlying, underlying + 5, dte, vol, interest)).not.toBeNaN();
+        expect(calc_price_put(underlying, underlying + 5, dte, vol, interest)).not.toBeNaN();
     });
     test("delta", () => {
-        expect(calc_delta_call(underlying, strike, dte, vol, interest)).toBe(0.9483873825790621);
-        expect(calc_delta_put(underlying, strike, dte, vol, interest)).toBe(-0.05161261742093792);
+        expect(calc_delta_call(underlying, underlying - 5, dte, vol, interest)).toBe(0.9483873825790621);
+        expect(calc_delta_put(underlying, underlying - 5, dte, vol, interest)).toBe(-0.05161261742093792);
+        expect(calc_delta_call(underlying, underlying, dte, vol, interest)).not.toBeNaN();
+        expect(calc_delta_put(underlying, underlying, dte, vol, interest)).not.toBeNaN();
+        expect(calc_delta_call(underlying, underlying + 5, dte, vol, interest)).not.toBeNaN();
+        expect(calc_delta_put(underlying, underlying + 5, dte, vol, interest)).not.toBeNaN();
     });
     test("gamma", () => {
-        expect(calc_gamma(underlying, strike, dte, vol, interest)).toBe(0.03195231728952847);
+        expect(calc_gamma(underlying, underlying - 5, dte, vol, interest)).toBe(0.03195231728952847);
+        expect(calc_gamma(underlying, underlying, dte, vol, interest)).not.toBeNaN();
+        expect(calc_gamma(underlying, underlying + 5, dte, vol, interest)).not.toBeNaN();
     });
     test("theta", () => {
-        expect(calc_theta_call(underlying, strike, dte, vol, interest)).toBe(-0.004724955703733377);
-        expect(calc_theta_put(underlying, strike, dte, vol, interest)).toBe(-0.004355214268847118);
+        expect(calc_theta_call(underlying, underlying - 5, dte, vol, interest)).toBe(-0.004724955703733377);
+        expect(calc_theta_put(underlying, underlying - 5, dte, vol, interest)).toBe(-0.004355214268847118);
+        expect(calc_theta_call(underlying, underlying, dte, vol, interest)).not.toBeNaN();
+        expect(calc_theta_put(underlying, underlying, dte, vol, interest)).not.toBeNaN();
+        expect(calc_theta_call(underlying, underlying + 5, dte, vol, interest)).not.toBeNaN();
+        expect(calc_theta_put(underlying, underlying + 5, dte, vol, interest)).not.toBeNaN();
     });
     test("vega", () => {
-        expect(calc_vega(underlying, strike, dte, vol, interest)).toBe(0.017508119062755328);
+        expect(calc_vega(underlying, underlying - 5, dte, vol, interest)).toBe(0.017508119062755328);
+        expect(calc_vega(underlying, underlying, dte, vol, interest)).not.toBeNaN();
+        expect(calc_vega(underlying, underlying + 5, dte, vol, interest)).not.toBeNaN();
     });
     test("rho", () => {
-        expect(calc_rho_call(underlying, strike, dte, vol, interest)).toBe(0.046390125072606016);
-        expect(calc_rho_put(underlying, strike, dte, vol, interest)).toBe(-0.002908732912228553);
+        expect(calc_rho_call(underlying, underlying - 5, dte, vol, interest)).toBe(0.046390125072606016);
+        expect(calc_rho_put(underlying, underlying - 5, dte, vol, interest)).toBe(-0.002908732912228553);
+        expect(calc_rho_call(underlying, underlying, dte, vol, interest)).not.toBeNaN();
+        expect(calc_rho_put(underlying, underlying, dte, vol, interest)).not.toBeNaN();
+        expect(calc_rho_call(underlying, underlying + 5, dte, vol, interest)).not.toBeNaN();
+        expect(calc_rho_put(underlying, underlying + 5, dte, vol, interest)).not.toBeNaN();
     });
 });
 
@@ -38,9 +58,17 @@ describe("calculate IV", () => {
     const interest = 0.01;
     const dte = 30;
 
-    const callPrice = 2.30;
+    const price = 2.30;
 
-    test("IV", () => {
-        expect(calc_iv(underlying, strike, dte, interest, callPrice)).toBe(0.3688563249143389);
+    test("IV Call", () => {
+        expect(calc_iv_call(underlying, 105, dte, interest, price)).toBe(0.3688563249143389);
+        expect(calc_iv_call(underlying, 100, dte, interest, price)).toBe(0.19759119494593871);
+        // expect(calc_iv_call(underlying, 95, dte, interest, price)).toBe(0.3688563249143389);
+    });
+
+    test("IV Put", () => {
+        expect(calc_iv_put(underlying, 95, dte, interest, price)).toBe(0.3931007096285325);
+        expect(calc_iv_put(underlying, 100, dte, interest, price)).toBe(0.20478119460977465);
+        // expect(calc_iv_put(underlying, 105, dte, interest, price)).toBe(0.3931007096285325);
     });
 });
