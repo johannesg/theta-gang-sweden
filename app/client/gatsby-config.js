a272b94f60d0f474eab1598ee5005db8e119bb7b
Johannes Gustafsson <johannes@jogus.io>
2021-09-15 14:36:48 +0200

    add google analytics

--- app/client/gatsby-config.js
+++ app/client/gatsby-config.js
@@ -28,5 +28,17 @@ module.exports = {
     // If you want to use styled components you should add the plugin here.
     // 'gatsby-plugin-styled-components',
     'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-TMLRDF7P4Z"
+          // "GA-TRACKING_ID", // Google Analytics / GA
+          // "AW-CONVERSION_ID", // Google Ads / Adwords / AW
+          // "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+        ],
+      },
+    },
   ],
 };
