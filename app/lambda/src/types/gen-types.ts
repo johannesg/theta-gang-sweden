7d1ef103e79cd84175cb8f61a6bafa11126b8b31
Johannes Gustafsson <johannesgu@gmail.com>
2022-04-03 15:37:41 +0200

    npm update

--- app/lambda/src/types/gen-types.ts
+++ app/lambda/src/types/gen-types.ts
@@ -1,10 +1,11 @@
 import { GraphQLResolveInfo } from 'graphql';
 import { ContextWithDataSources } from './';
 export type Maybe<T> = T | null;
+export type InputMaybe<T> = Maybe<T>;
 export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
 export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
 export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
-export type RequireFields<T, K extends keyof T> = { [X in Exclude<keyof T, K>]?: T[X] } & { [P in K]-?: NonNullable<T[P]> };
+export type RequireFields<T, K extends keyof T> = Omit<T, K> & { [P in K]-?: NonNullable<T[P]> };
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
   ID: string;
@@ -109,7 +110,7 @@ export type Query = {
 export type QueryMatrixArgs = {
   expires: Scalars['String'];
   id: Scalars['ID'];
-  includeDetails?: Maybe<Scalars['Boolean']>;
+  includeDetails?: InputMaybe<Scalars['Boolean']>;
   type: OptionType;
 };
 
@@ -141,7 +142,7 @@ export type SubscriptionSubscribeFn<TResult, TParent, TContext, TArgs> = (
   args: TArgs,
   context: TContext,
   info: GraphQLResolveInfo
-) => AsyncIterator<TResult> | Promise<AsyncIterator<TResult>>;
+) => AsyncIterable<TResult> | Promise<AsyncIterable<TResult>>;
 
 export type SubscriptionResolveFn<TResult, TParent, TContext, TArgs> = (
   parent: TParent,
