61a3bf2fcbf3602a6f398ee83630a90baf0bdaec
Johannes Gustafsson <johannes@jogus.io>
2023-09-15 16:57:47 +0200

    beth stack

--- app2/src/app.tsx
+++ app2/src/app.tsx
@@ -28,18 +28,25 @@ export const app = new Elysia()
             <p class="text-white tracking-wide  font-medium font-sans text-xl">Theta Gang Sweden</p>
         </div>
 
-        <div class="p-4">
-            Filters
-            <Dropdown options={options}>
-            </Dropdown>
+        <div class="p-4" hx-get="/instruments" hx-trigger="load">
+            <Dropdown label="Loading"></Dropdown>
         </div>
-        <div>
+        <div id="options">
         <p>HEJ</p>
         </div>
         </body> 
       </BaseHtml>
     )
   )
+  .get("/instruments", ( { html }) => 
+          html(
+              <Dropdown label="Instrument">
+                  <DropdownItem hxGet="/instrument/one" hxTarget="#options"> One </DropdownItem>
+                  <DropdownItem hxGet="/instrument/two" hxTarget="#options"> Two </DropdownItem>
+                  <DropdownItem hxGet="/instrument/three" hxTarget="#options"> Three </DropdownItem>
+              </Dropdown>
+              )
+      )
   .get("/styles.css", () => Bun.file("./tailwind-gen/styles.css"));
 
 
@@ -62,19 +69,42 @@ const BaseHtml = ({ children }: elements.Children) => `
 ${children}
 `;
 
-function Dropdown({ options }: { options: Option[]}) {
+type DropdownProps = elements.Children & {
+    label: string
+    children: typeof DropdownItem
+}
+
+type DropdownItemProps = elements.Children & {
+    hxGet: string
+    hxTarget: string
+}
+
+function DropdownItem({ hxGet, hxTarget, children }: DropdownItemProps) {
+    return <li>
+      <a
+        class="block w-full whitespace-nowrap bg-transparent px-4 py-2 text-sm font-normal text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400 dark:text-neutral-200 dark:hover:bg-neutral-600"
+        hx-get={hxGet}
+        hx-target={hxTarget}
+        data-te-dropdown-item-ref
+        _="on click set { innerText: my.innerText} on the previous <span[data-dropdown-label] /> "
+        >{children}</a>
+    </li>
+}
+
+function Dropdown({ label, children }: DropdownProps) {
     return <div class="relative" data-te-dropdown-ref>
   <button
     class="flex items-center whitespace-nowrap rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] motion-reduce:transition-none dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
     type="button"
-    id="dropdownMenuButton1"
     data-te-dropdown-toggle-ref
     aria-expanded="false"
     data-te-ripple-init
     data-te-ripple-color="light"
-    _="on click toggle .hidden on the next <ul/>"
+    _="on click 
+        toggle .hidden on the next <ul/>
+        halt"
     >
-    Dropdown button
+    <span data-dropdown-label>{label}</span>
     <span class="ml-2 w-2">
       <svg
         xmlns="http://www.w3.org/2000/svg"
@@ -91,31 +121,10 @@ function Dropdown({ options }: { options: Option[]}) {
   <ul
     class="absolute z-[1000] float-left m-0 hidden min-w-max list-none overflow-hidden rounded-lg border-none bg-white bg-clip-padding text-left text-base shadow-lg dark:bg-neutral-700 [&[data-te-dropdown-show]]:block"
     aria-labelledby="dropdownMenuButton1"
-    data-te-dropdown-menu-ref>
-    <li>
-      <a
-        class="block w-full whitespace-nowrap bg-transparent px-4 py-2 text-sm font-normal text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400 dark:text-neutral-200 dark:hover:bg-neutral-600"
-        href="#"
-        data-te-dropdown-item-ref
-        >Action</a
-      >
-    </li>
-    <li>
-      <a
-        class="block w-full whitespace-nowrap bg-transparent px-4 py-2 text-sm font-normal text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400 dark:text-neutral-200 dark:hover:bg-neutral-600"
-        href="#"
-        data-te-dropdown-item-ref
-        >Another action</a
-      >
-    </li>
-    <li>
-      <a
-        class="block w-full whitespace-nowrap bg-transparent px-4 py-2 text-sm font-normal text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400 dark:text-neutral-200 dark:hover:bg-neutral-600"
-        href="#"
-        data-te-dropdown-item-ref
-        >Something else here</a
-      >
-    </li>
+    data-te-dropdown-menu-ref
+    _="on click from body if I do not match .hidden add .hidden"
+    >
+    {children}
   </ul>
 </div>
     // return <select>
